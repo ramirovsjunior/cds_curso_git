7ec77a30f494d103c4bca8589966bfbb6bcc5de5
ramirovsjunior <ramirovsjunior@gmail.com>
2022-12-13 16:09:57 -0300

    Efetuado carga de dados e apresentado dataframe

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+
+def load_data():
+    return pd.read_csv('/Users/ramirovsjunior/Documents/repos/curso_git/projeto/data/processed/bikes_completed.csv')
+
+
+df = load_data()
+
+st.dataframe(df)
